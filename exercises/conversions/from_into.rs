7e06cf2f40ef90f993fcc6be140281fa80c15abb
Prem Pillai <prem.pillai@gmail.com>
2021-05-29 10:26:03 +1000

    until "From" trait done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,36 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        match s.len() {
+            0 => Default::default(),
+            _ => {
+                let input: Vec<&str> = s.splitn(2, ",").collect();
+                let name = input.get(0);
+                let age = input.get(1);
+                match name {
+                    Some(n) => {
+                        if n.is_empty() {
+                            return Default::default();
+                        }
+                    }
+                    None => return Default::default(),
+                }
+                match age {
+                    Some(a) => {
+                        return match a.parse::<usize>() {
+                            Ok(ag) => Person {
+                                name: name.unwrap().to_owned().to_string(),
+                                age: ag,
+                            },
+                            _ => Default::default(),
+                        }
+                    }
+                    None => return Default::default(),
+                }
+            }
+        }
     }
 }
 
